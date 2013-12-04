5729cd2b8c19674660db0678777b22081a594bf5
John Hobbs <john@velvetcache.org>
2013-12-04 00:53:35 -0600

    Config loading.

--- dundunduh/__init__.py
+++ dundunduh/__init__.py
@@ -2,12 +2,29 @@
 
 import os
 from flask import Flask
+
+from .config import BaseConfig
 from .views import register_views
+from .extensions import rq
 
 app = Flask(__name__)
 
+##############################
+# Load Config
+
 app.config['UPLOAD_FOLDER'] = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'uploads')
-app.config['MAX_CONTENT_LENGTH'] = 4 * 1024 * 1024
-app.config['LONGEST_SIDE'] = 400
+
+app.config.from_object(BaseConfig)
+app.config.from_envvar('CONFIG', silent=True)
+
+app.debug = app.config.get('DEBUG', False)
+
+##############################
+# Attach Views
 
 register_views(app)
+
+##############################
+# Init Extensions
+
+rq.init_app(app)
---
+++ dundunduh/config.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+
+class BaseConfig(object):
+    ENV = 'PRODUCTION'
+
+    DEBUG = False
+    TESTING = False
+
+    REDIS_URL = 'redis://localhost:6379/0'
+
+    AWS_ACCESS_KEY = None
+    AWS_SECRET_KEY = None
+
+    # Max POST request size
+    MAX_CONTENT_LENGTH = 6 * 1024 * 1024  # 6MB
+
+    # Size in pixels to crop images
+    LONGEST_SIDE = 400
+
+
+class TestConfig(BaseConfig):
+    ENV = 'TESTING'
+
+    TESTING = True
+    CSRF_ENABLED = False
+
+    REDIS_URL = 'redis://localhost:6379/15'
